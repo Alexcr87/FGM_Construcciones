06c2b45ef6d210568ad169c95766b4b1ae4720ec
TomasKMorelli <totimorelli123@gmail.com>
2025-07-03 12:03:08 -0300

    c

---
+++ Back/fgm-materiales/chirs.ts
@@ -0,0 +1,419 @@
+export const categorias = [
+    {
+        name: 'Atlantis Decor',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: "",
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,36 m²',
+        dimension: '64x123cm',
+        material: 'Aceitado',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+    {
+        name: 'Berton Nero Lux Mag',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento..',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,16 m²',
+        dimension: '85x85cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+    {
+        name: 'Calacata Ibiza Mag',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,16 m²',
+        dimension: '86x86cm',
+        material: 'Aceitado',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+    {
+        name: 'Calacata Ibiza Lux Mag ',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2.16m2',
+        dimension: '85x85cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+
+    {
+        name: 'Collin Lux Mag',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2.16m2',
+        dimension: '85x85cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+
+    {
+        name: 'Collin Lux Ultra',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2.00m2',
+        dimension: '100x100cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+    {
+        name: 'Marmo Allure Lux',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,81 m²',
+        dimension: '75x75cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+    {
+        name: 'Marmo Venus',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: ' 2,52 m²',
+        dimension: '60x60cm',
+        material: 'brillante',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+           
+        ],
+    },
+
+
+
+    {
+        name: 'Marmo Gris Lux',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: ' 2,81 m² m²',
+        dimension: '75x75cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+          
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+    {
+        name: 'Mont Blanc',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,87 m²',
+        dimension: '76x76cm',
+        material: 'Aceitado',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+          
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+
+    {
+        name: 'Mont Blanc Polido',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,81 m²',
+        dimension: '75x75cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+          
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+
+    {
+        name: 'Ônix Cristallo Lux Ultra',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,00 m²',
+        dimension: '100x100cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+          
+        ],
+    },
+
+
+
+    {
+        name: 'Ônix Ivory',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,52 m²',
+        dimension: '60x60cm',
+        material: 'Brillante',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+        
+        ],
+    },
+
+
+
+    {
+        name: 'Ônix Perla Ultra',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,00 m²',
+        dimension: '100x100cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+          
+        ],
+    },
+
+
+    {
+        name: 'Oregon Off White Lux',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,81 m²',
+        dimension: ' 75x75cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+
+
+
+    {
+        name: 'Pettros Beige Out',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,52 m²',
+        dimension: ' 60x60cm',
+        material: 'Safe',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+
+    {
+        name: 'Pettros Gray out',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,52 m²',
+        dimension: ' 60x60cm',
+        material: 'Safe',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+        ],
+    },
+
+    {
+        name: 'Sevilha Decor',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,36 m²',
+        dimension: ' 64x123cm',
+        material: 'Aceitado',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+           
+        ],
+    },
+
+
+
+    {
+        name: 'Tiffany Mag Lux',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,16 m²',
+        dimension: ' 85x85cm',
+        material: 'Pulido',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+           
+        ],
+    },
+
+
+
+    {
+        name: 'Trento Gray Out',
+        description: 'Ambientes residenciales y comerciales con alto tráfico de personas. Recomendado para lugares donde no se requiere resistencia al deslizamiento.',
+        img: '',
+        is_active: true,
+        surface: 'Pisos y Paredes',
+        Box: '2,52 m²',
+        dimension: ' 60x60cm',
+        material: 'Safe',
+        categories: [
+            {
+                name: 'Porcelanatos y Porcelanicos',
+            },
+            {
+                name: 'Pisos y Revestimientos',
+            },
+           
+        ],
+    },
+
+
+
+
+
+
+]

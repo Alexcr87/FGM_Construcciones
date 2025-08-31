484d856a8560a8847e876016718007adf2d849db
Christian <alexcr87@live.com.ar>
2025-08-31 14:05:56 -0300

    dropschema false cors habilitado para vercel

--- Back/fgm-materiales/src/main.ts
+++ Back/fgm-materiales/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      "http://localhost:3000",
+      "https://fgm-construcciones.vercel.app"
+    ],
   });
 
   const swaggerConfig = new DocumentBuilder()
